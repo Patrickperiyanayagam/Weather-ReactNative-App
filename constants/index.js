7f12012b2989664e0362aa7a79b95a70acd35a57
Patrickperiyanayagam <patrickperiyanayagam31@gmail.com>
2024-06-12 20:54:07 +0530

    first commit

---
+++ constants/index.js
@@ -0,0 +1,20 @@
+export const apiKey = "2ae7404a6dd04b94a36120340240605"
+
+export const weatherImages = {
+    "Partly cloudy" : require("../assets/partlycloudy.png"),
+    "Moderate rain" : require("../assets/moderaterain.png"),
+    "Patchy rain possible" : require("../assets/moderaterain.png"),
+    "Patchy rain nearby" : require("../assets/moderaterain.png"),
+    "Sunny" : require("../assets/sun.png"),
+    "Clear" : require("../assets/sun.png"),
+    "Overcast" : require("../assets/cloud.png"),
+    "Cloudy" : require("../assets/cloud.png"),
+    "Light rain" : require("../assets/moderaterain.png"),
+    "Moderate rain at times" : require("../assets/moderaterain.png"),
+    "Heavy rain":require("../assets/heavyrain.png"),
+    "Heavy rain at times":require("../assets/heavyrain.png"),
+    "Moderate or heavy freezing rain":require("../assets/heavyrain.png"),
+    "Moderate or heavy rain with thunder":require("../assets/heavyrain.png"),
+    "Mist":require("../assets/mist.png"),
+    "other":require("../assets/moderaterain.png")
+}
